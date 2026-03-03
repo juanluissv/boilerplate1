cd397794983ee0154bf5882a8d3e70d4128d1ff3
juan <juanluissv@juans-MacBook-Air.local>
2026-03-03 11:40:51 -0600

    initial setup

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import router from './Routes';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+          <RouterProvider router={router} />
+    </Provider>
+);
+
+
---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import path from 'path'
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+
+dotenv.config()
+
+//connectDB()
+
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const __dirname = path.resolve() //set  __dirname to current directory
+
+
+app.get('/', (req, res) => {
+    res.send("API is running...")
+})
+
+
+
+
+
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 8100
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
